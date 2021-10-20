c3d6f03d1052525807dc51b982b2e805900197a1
Artus U <artusrock@hotmail.com>
2021-10-20 14:41:45 +0100

    limiting fetch to 10 currencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -5,31 +5,36 @@ import { Card, Row, Col, Input } from "antd";
 
 import { useGetCryptosQuery } from "./services/cryptoApi";
 
-const Cryptocurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
-  console.log(cryptos)
+  console.log(cryptos);
 
+  if (isFetching) return 'Loading...'
 
   return (
     <>
-     <Row gutter={[32, 32]} className="crypto-card-container">
-       {cryptos.map((currency) => (
-         <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-           <Link key={currency.id} to={`/crypto/${currency.id}`}>
-             <Card title={`${currency.id}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} /> } hoverable>
-             <p>Price: {millify(currency.price)}</p>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link key={currency.id} to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.id}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
                 <p>Daily Change: {currency.change}%</p>
-             </Card>
-           </Link>
-
-         </Col>
-       ))}
-     </Row>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
     </>
-  )
+  );
 };
 
 export default Cryptocurrencies;
