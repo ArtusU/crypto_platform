96a12bf12d5a9c92f80a95d58e30ebe68691376c
Artus U <artusrock@hotmail.com>
2021-10-21 10:56:07 +0100

    cryptoNewsApi.js fetching bing news api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
-const News = () => {
+import { useGetCryptoNewsQuery } from "../components/services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 12,
+  });
+  console.log(cryptoNews)
   return <div>News</div>;
 };
 
